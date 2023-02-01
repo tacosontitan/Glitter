c186a728acfb4de19db1bba4f0744e0a113bd911
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-01 16:53:20 -0600

    Created sql query tests.

---
+++ test/Glitter.Tests/Sql/SqlQueryTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Glitter.Sql;
+
+namespace Glitter.Tests.Sql;
+
+public class SqlQueryTests
+{
+    [Fact]
+    public void Query()
+    {
+        var request = new SqlQuery("SELECT * FROM [dbo].[dummy]");
+        var provider = new MockProvider();
+        try
+        {
+            _ = provider.Query<object>(request);
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
+    }
+    [Fact]
+    public void Scalar()
+    {
+        var request = new SqlQuery("SELECT * FROM [dbo].[dummy]");
+        var provider = new MockProvider();
+        try
+        {
+            _ = provider.ExecuteScalar<object>(request);
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
+    }
+    [Fact]
+    public void Execute()
+    {
+        var request = new SqlQuery("SELECT * FROM [dbo].[dummy]");
+        var provider = new MockProvider();
+        try
+        {
+            provider.Execute(request);
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
+    }
+}
