50502cce1a23ab56bb0ef4c366d7af3ae4b825d4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-01 22:55:09 -0600

    Added sql column attribute.

---
+++ src/Glitter/Sql/SqlColumnAttribute.cs
@@ -0,0 +1,51 @@
+namespace Glitter.Sql;
+
+/// <summary>
+/// Represents an <see cref="Attribute"/> for marking properties as SQL columns.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public sealed class SqlColumnAttribute : Attribute
+{
+    /// <summary>
+    /// The name of the column.
+    /// </summary>
+    public string Name { get; private set; }
+    /// <summary>
+    /// The type of the column.
+    /// </summary>k
+    public DbType? Type { get; private set; }
+    /// <summary>
+    /// The size of the column.
+    /// </summary>
+    public int? Size { get; private set; }
+    /// <summary>
+    /// The precision of the column.
+    /// </summary>
+    public byte? Precision { get; private set; }
+    /// <summary>
+    /// The scale of the column.
+    /// </summary>
+    public byte? Scale { get; private set; }
+    /// <summary>
+    /// Creates a new <see cref="SqlColumnAttribute"/> instance.
+    /// </summary>
+    /// <param name="name">The name of the column.</param>
+    public SqlColumnAttribute(string name) =>
+        Name = name;
+    /// <summary>
+    /// Creates a new <see cref="SqlColumnAttribute"/> instance.
+    /// </summary>
+    /// <param name="name">The name of the column.</param>
+    /// <param name="type">The type of the column.</param>
+    /// <param name="size">The size of the column.</param>
+    /// <param name="precision">The precision of the column.</param>
+    /// <param name="scale">The scale of the column.</param>
+    public SqlColumnAttribute(string name, DbType type, int? size = null, byte? precision = null, byte? scale = null) :
+        this(name)
+    {
+        Type = type;
+        Size = size;
+        Precision = precision;
+        Scale = scale;
+    }
+}
