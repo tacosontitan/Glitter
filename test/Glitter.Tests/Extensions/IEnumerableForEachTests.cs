bcd299dc31a96ed6494b0f60276c888bc5a743ce
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-04 14:53:17 -0600

    Added for each tests.

---
+++ test/Glitter.Tests/Extensions/IEnumerableForEachTests.cs
@@ -0,0 +1,28 @@
+using Glitter.Extensions;
+
+namespace Glitter.Tests.Extensions;
+
+public class IEnumerableForEachTests
+{
+    [Fact]
+    public void SourceIsNull()
+    {
+        IEnumerable<int>? source = null;
+        Assert.Throws<ArgumentNullException>(() => source!.ForEach(_ => { }));
+    }
+    [Fact]
+    public void ActionIsNull()
+    {
+        IEnumerable<int>? source = Enumerable.Range(0, 10);
+        Assert.Throws<ArgumentNullException>(() => source!.ForEach(null!));
+    }
+    [Fact]
+    public void EachElementIsUtilized()
+    {
+        IEnumerable<int>? source = Enumerable.Range(0, 10);
+        if (source is null)
+            Assert.Fail("Source is null.");
+
+        source.ForEach(i => Assert.Equal(i, source.ElementAt(i)));
+    }
+}
