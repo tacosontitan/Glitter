e8c5252299845243435bf04040cd47e260827a79
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-13 21:12:01 -0600

    Updated user model to match the sample table definition.

--- samples/sql/Glitter.Samples.Sql/Users/User.cs
+++ samples/sql/Glitter.Samples.Sql/Users/User.cs
@@ -16,8 +16,18 @@ internal sealed class User
     [SqlColumn("Id", DbType.Int32)]
     public int Id { get; set; }
     /// <summary>
-    /// The name of the user.
+    /// The given name of the user.
     /// </summary>
-    [SqlColumn("Name", DbType.String, size: 255)]
-    public string Name { get; set; }
+    [SqlColumn("GivenName", DbType.String, size: 50)]
+    public string? GivenName { get; set; }
+    /// <summary>
+    /// The surname of the user.
+    /// </summary>
+    [SqlColumn("Surname", DbType.String, size: 50)]
+    public string? Surname { get; set; }
+    /// <summary>
+    /// The username of the user.
+    /// </summary>
+    [SqlColumn("Username", DbType.String, size: 50)]
+    public string? Username { get; set; }
 }
