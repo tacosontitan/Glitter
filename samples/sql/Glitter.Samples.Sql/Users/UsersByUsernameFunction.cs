21fcd1d120517f5386878db3f7365044f099432a
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-13 21:25:17 -0600

    Created encapsulation of the sample users query function.

---
+++ samples/sql/Glitter.Samples.Sql/Users/UsersByUsernameFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+using Glitter.Sql;
+
+namespace Glitter.Samples.Sql.Users
+{
+    /// <summary>
+    /// A <see cref="SqlFunction"/> representing the table valued function <c>UsersByUsernameQuery</c>.
+    /// </summary>
+    internal sealed class UsersByUsernameFunction : SqlFunction
+    {
+        /// <summary>
+        /// Creates a new <see cref="UsersByUsernameFunction"/> instance.
+        /// </summary>
+        /// <param name="request">The request for invoking the function.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><see cref="UsersByUsernameRequest.Username"/> is <see langword="null"/> or whitespace.</exception>
+        public UsersByUsernameFunction(UsersByUsernameRequest request) :
+            base("UsersByUsernameQuery")
+        {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+
+            if (string.IsNullOrWhiteSpace(request.Username))
+                throw new ArgumentException($"The specified username `{request.Username}` is invalid.");
+
+            _ = AddParameter("Username", request.Username, DbType.String, size: 50);
+        }
+    }
+}
