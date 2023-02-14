b0c697732b1fc0792498e29c4b8b13771bb49c8b
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-13 21:08:24 -0600

    Created a basic username query function.

---
+++ samples/sql/Glitter.Samples.Sql.Database/Functions/UsersQueryByUsername.sql
@@ -0,0 +1,21 @@
+﻿CREATE FUNCTION [dbo].[UsersQueryByUsername]
+(
+    @Username VARCHAR(50)
+)
+RETURNS @Results TABLE
+(
+    Id INT,
+    Username VARCHAR(50),
+    GivenName VARCHAR(50),
+    Surname VARCHAR(50)
+)
+AS
+BEGIN
+    INSERT @Results
+    SELECT U.Id,
+           U.Username,
+           U.GivenName,
+           U.Surname
+    FROM [dbo].[Users] U
+    RETURN
+END
