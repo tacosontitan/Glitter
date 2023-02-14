8f0a68ada3b2058f6e224dbe143318a7b59ac575
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-13 21:04:18 -0600

    Added a stored procedure for inserting users.

---
+++ samples/sql/Glitter.Samples.Sql.Database/Stored Procedures/UserInsert.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[UserInsert]
+    @Username VARCHAR(50),
+    @GivenName VARCHAR(50),
+    @Surname VARCHAR(50)
+AS
+
+    -- Insert the user.
+    INSERT INTO [dbo].[Users] (
+        Username,
+        GivenName,
+        Surname
+    ) VALUES (
+        @Username,
+        @GivenName,
+        @Surname
+    );
+
+    -- Capture the identity of the newly created record.
+    DECLARE @Id INT = SCOPE_IDENTITY();
+
+    -- Select the newly created record.
+    SELECT Username,
+           GivenName,
+           Surname
+    FROM [dbo].[Users] U
+    WHERE Id = @Id;
+
+-- Return the identity of the newly created record.
+RETURN CASE
+    WHEN @Id > 0
+    THEN @Id
+    ELSE -1
+END
