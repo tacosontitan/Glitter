ed2d27b8712c6a07b2509bcbbd859b25f7b07ec7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 07:40:20 -0600

    Created not found exception.

---
+++ src/Glitter/Validation/NotFoundException.cs
@@ -0,0 +1,23 @@
+namespace Glitter.Validation;
+
+/// <summary>
+/// Represents an exception that is thrown when a resource is not found.
+/// </summary>
+public sealed class NotFoundException : Exception
+{
+    /// <summary>
+    /// Gets additional information about the exception.
+    /// </summary>
+    public string? AdditionalInformation { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+    /// </summary>
+    public NotFoundException() : this(null) { }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+    /// </summary>
+    /// <param name="addtionalInformation">Additional information about the exception.</param>
+    public NotFoundException(string? addtionalInformation) :
+        base("The requested resource was not found.") =>
+        AdditionalInformation = addtionalInformation;
+}
