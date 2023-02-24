7734798224b49282b2195c359c2ed500d3f758b6
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 13:49:15 -0600

    Created enqueue extensions.

---
+++ src/Glitter.Extensions/Collections/EnqueueExtensions.cs
@@ -0,0 +1,54 @@
+namespace Glitter.Extensions.Collections;
+
+/// <summary>
+/// Defines a collection of extension methods.
+/// </summary>
+public static class EnqueueExtensions
+{
+    /// <summary>
+    /// Enqueues a collection of elements to the queue.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the queue.</typeparam>
+    /// <param name="source">The queue to enqueue to.</param>
+    /// <param name="items">The elements to enqueue.</param>
+    /// <returns>The queue.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="source"/> or <paramref name="items"/> is <see langword="null"/>.
+    /// </exception>
+    public static Queue<T> Enqueue<T>(this Queue<T> source, IEnumerable<T> items)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (items is null)
+            throw new ArgumentNullException(nameof(items));
+
+        foreach (T item in items)
+            source.Enqueue(item);
+
+        return source;
+    }
+    /// <summary>
+    ///  Enqueues a collection of elements to the queue.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the queue.</typeparam>
+    /// <param name="source">The queue to enqueue to.</param>
+    /// <param name="items">The elements to enqueue.</param>
+    /// <returns>The queue.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="source"/> or <paramref name="items"/> is <see langword="null"/>.
+    /// </exception>
+    public static Queue<T> Enqueue<T>(this Queue<T> source, params T[] items)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (items is null)
+            throw new ArgumentNullException(nameof(items));
+
+        foreach (T item in items)
+            source.Enqueue(item);
+
+        return source;
+    }
+}
