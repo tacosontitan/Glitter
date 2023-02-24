2930b400eb3daf971ca73eb6bfa68ee1a3774ec5
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 10:24:46 -0600

    Commented out enumerable extensions tests.

--- test/Glitter.Tests/Extensions/IEnumerableForEachTests.cs
+++ test/Glitter.Tests/Extensions/IEnumerableForEachTests.cs
@@ -1,25 +1,25 @@
-using Glitter.Extensions;
+//using Glitter.Extensions;
 
-namespace Glitter.Tests.Extensions;
+//namespace Glitter.Tests.Extensions;
 
-public class IEnumerableForEachTests
-{
-    [Fact]
-    public void SourceIsNull()
-    {
-        IEnumerable<int>? source = null;
-        Assert.Throws<ArgumentNullException>(() => source!.ForEach(_ => { }));
-    }
-    [Fact]
-    public void ActionIsNull()
-    {
-        IEnumerable<int> source = Enumerable.Range(0, 10);
-        Assert.Throws<ArgumentNullException>(() => source!.ForEach(null!));
-    }
-    [Fact]
-    public void EachElementIsUtilized()
-    {
-        IEnumerable<int> source = Enumerable.Range(0, 10);
-        source.ForEach(i => Assert.Equal(i, source.ElementAt(i)));
-    }
-}
+//public class IEnumerableForEachTests
+//{
+//    [Fact]
+//    public void SourceIsNull()
+//    {
+//        IEnumerable<int>? source = null;
+//        Assert.Throws<ArgumentNullException>(() => source!.ForEach(_ => { }));
+//    }
+//    [Fact]
+//    public void ActionIsNull()
+//    {
+//        IEnumerable<int> source = Enumerable.Range(0, 10);
+//        Assert.Throws<ArgumentNullException>(() => source!.ForEach(null!));
+//    }
+//    [Fact]
+//    public void EachElementIsUtilized()
+//    {
+//        IEnumerable<int> source = Enumerable.Range(0, 10);
+//        source.ForEach(i => Assert.Equal(i, source.ElementAt(i)));
+//    }
+//}
