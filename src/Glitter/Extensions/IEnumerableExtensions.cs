9d2c169300ec3faad108b8457c7062eb3bdfc017
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 07:33:02 -0600

    Created index of extension method.

--- src/Glitter/Extensions/IEnumerableExtensions.cs
+++ src/Glitter/Extensions/IEnumerableExtensions.cs
@@ -25,10 +25,10 @@ public static class IEnumerableExtensions
     {
         if (collection is null)
             throw new ArgumentNullException(nameof(collection));
-        
+
         if (selector is null)
             throw new ArgumentNullException(nameof(selector));
-        
+
         if (!collection.Any())
             return Enumerable.Empty<TResult>();
 
@@ -56,5 +56,34 @@ public static class IEnumerableExtensions
 
         foreach (var item in source)
             action(item);
+    }
+    /// <summary>
+    /// Gets the index of the specified search value in the collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <param name="collection">The collection to iterate over.</param>
+    /// <param name="searchValue">The value to search for.</param>
+    /// <returns>The index of the specified search value in the collection, or -1 if the search value is not found.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="collection"/> or <paramref name="searchValue"/> is <see langword="null"/>.
+    /// </exception>
+    public static int IndexOf<T>(this IEnumerable<T> collection, T searchValue)
+    {
+        if (collection is null)
+            throw new ArgumentNullException(nameof(collection));
+        
+        if (searchValue is null)
+            throw new ArgumentNullException(nameof(searchValue));
+        
+        int index = 0;
+        foreach (T item in collection)
+        {
+            if (item?.Equals(searchValue) == true)
+                return index;
+
+            index++;
+        }
+
+        return -1;
     }
 }
