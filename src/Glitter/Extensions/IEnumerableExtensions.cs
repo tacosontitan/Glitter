f39a58af67d24701403bb351fb9ea5bfef1682b0
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 07:48:17 -0600

    Created before extension method.

--- src/Glitter/Extensions/IEnumerableExtensions.cs
+++ src/Glitter/Extensions/IEnumerableExtensions.cs
@@ -34,6 +34,29 @@ public static class IEnumerableExtensions
             : source.Skip(index + 1);
     }
     /// <summary>
+    /// Returns a collection of elements from the specified collection that occur before the specified search value.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <param name="source">The collection to iterate over.</param>
+    /// <param name="searchValue">The value to search for.</param>
+    /// <returns>A collection of elements from the specified collection that occur before the specified search value.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="source"/> or <paramref name="searchValue"/> is <see langword="null"/>.
+    /// </exception>
+    public static IEnumerable<T> Before<T>(this IEnumerable<T> source, T searchValue)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (searchValue is null)
+            throw new ArgumentNullException(nameof(searchValue));
+
+        int index = source.IndexOf(searchValue);
+        return index == -1
+            ? Enumerable.Empty<T>()
+            : source.Take(index);
+    }
+    /// <summary>
     /// Executes the specified action on each element of the collection.
     /// </summary>
     /// <typeparam name="T">The type of the elements in the collection.</typeparam>
