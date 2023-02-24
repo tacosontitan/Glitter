6254c387bddaa6edcd77ac605f11de749fe302d7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 07:29:38 -0600

    Created select distinct extension method.

--- src/Glitter/Extensions/IEnumerableExtensions.cs
+++ src/Glitter/Extensions/IEnumerableExtensions.cs
@@ -11,6 +11,33 @@ namespace Glitter.Extensions;
 public static class IEnumerableExtensions
 {
     /// <summary>
+    /// Returns a collection of distinct elements from the specified collection, using the specified selector function to determine equality.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the result collection.</typeparam>
+    /// <param name="collection">The collection to iterate over.</param>
+    /// <param name="selector">The selector function to determine equality.</param>
+    /// <returns>A collection of distinct elements from the specified collection.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="collection"/> or <paramref name="selector"/> is <see langword="null"/>.
+    /// </exception>
+    public static IEnumerable<TResult> SelectDistinct<T, TResult>(this IEnumerable<T> collection, Func<T, TResult> selector)
+    {
+        if (collection is null)
+            throw new ArgumentNullException(nameof(collection));
+        
+        if (selector is null)
+            throw new ArgumentNullException(nameof(selector));
+        
+        if (!collection.Any())
+            return Enumerable.Empty<TResult>();
+
+        IEnumerable<TResult> selection = collection.Select(selector);
+        return selection is null
+            ? Enumerable.Empty<TResult>()
+            : selection.Distinct();
+    }
+    /// <summary>
     /// Executes the specified action on each element of the collection.
     /// </summary>
     /// <typeparam name="T">The type of the elements in the collection.</typeparam>
