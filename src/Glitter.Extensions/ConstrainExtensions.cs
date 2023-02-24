e14b01b4ce148bd5ded868cd5dc9e5e0638fa8e9
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 13:26:24 -0600

    Created constrain extension

---
+++ src/Glitter.Extensions/ConstrainExtensions.cs
@@ -0,0 +1,37 @@
+namespace Glitter.Extensions;
+
+/// <summary>
+/// Defines a set of extension methods for constraining values.
+/// </summary>
+public static class ConstrainExtensions
+{
+    /// <summary>
+    /// Constrains the specified input value to be within the specified bounds.
+    /// </summary>
+    /// <typeparam name="T">The type of the input value.</typeparam>
+    /// <param name="input">The input value.</param>
+    /// <param name="lowerBound">The lower bound.</param>
+    /// <param name="upperBound">The upper bound.</param>
+    /// <returns>The constrained value.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="input"/>, <paramref name="lowerBound"/>, or <paramref name="upperBound"/> is <see langword="null"/>.
+    /// </exception>
+    public static T Constrain<T>(this T input, T lowerBound, T upperBound) where T : IComparable
+    {
+        if (input is null)
+            throw new ArgumentNullException(nameof(input));
+        
+        if (lowerBound is null)
+            throw new ArgumentNullException(nameof(lowerBound));
+
+        if (upperBound is null)
+            throw new ArgumentNullException(nameof(upperBound));
+
+        if (input.CompareTo(lowerBound) < 0)
+            input = lowerBound;
+        else if (input.CompareTo(upperBound) > 0)
+            input = upperBound;
+
+        return input;
+    }
+}
