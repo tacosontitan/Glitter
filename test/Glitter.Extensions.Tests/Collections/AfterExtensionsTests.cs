c5c7474c5b74691d7cddfae06fc59a0c5567c322
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 20:21:23 -0600

    Created tests for the after extension method.

---
+++ test/Glitter.Extensions.Tests/Collections/AfterExtensionsTests.cs
@@ -0,0 +1,83 @@
+namespace Glitter.Extensions.Tests.Collections;
+
+public class AfterExtensionsTests
+{
+    [Fact]
+    public void SourceIsNull()
+    {
+        // Arrange
+        IEnumerable<int> source = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => source.After(1));
+    }
+    [Fact]
+    public void SearchValueIsNull()
+    {
+        // Arrange
+        IEnumerable<int?> source = new int?[] { 1, 2, 3 };
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => source.After(null));
+    }
+    [Fact]
+    public void SearchValueIsNotFound()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+
+        // Act
+        IEnumerable<int> result = source.After(4);
+
+        // Assert
+        Assert.Empty(result);
+    }
+    [Fact]
+    public void SearchValueIsFound()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+
+        // Act
+        IEnumerable<int> result = source.After(2);
+
+        // Assert
+        Assert.Equal(new[] { 3 }, result);
+    }
+    [Fact]
+    public void SearchValueIsFoundAtEnd()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+
+        // Act
+        IEnumerable<int> result = source.After(3);
+
+        // Assert
+        Assert.Empty(result);
+    }
+    [Fact]
+    public void SearchValueIsFoundAtBeginning()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+
+        // Act
+        IEnumerable<int> result = source.After(1);
+
+        // Assert
+        Assert.Equal(new[] { 2, 3 }, result);
+    }
+    [Fact]
+    public void SearchValueIsPresentMultipleTimes()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3, 2, 3 };
+
+        // Act
+        IEnumerable<int> result = source.After(2);
+
+        // Assert
+        Assert.Equal(new[] { 3, 2, 3 }, result);
+    }
+}
