23a254e131d7a5db50ce16801de612c4d243db53
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-24 21:29:28 -0600

    Added validation.

--- src/Glitter.Extensions/Collections/DequeueExtensions.cs
+++ src/Glitter.Extensions/Collections/DequeueExtensions.cs
@@ -15,6 +15,9 @@ public static class DequeueExtensions
     /// <exception cref="ArgumentNullException">
     /// <paramref name="source"/> is <see langword="null"/>.
     /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="source"/> is empty.
+    /// </exception>
     /// <exception cref="ArgumentOutOfRangeException">
     /// <paramref name="count"/> is negative.
     /// </exception>
@@ -25,6 +28,12 @@ public static class DequeueExtensions
         
         if (count < 0)
             throw new ArgumentOutOfRangeException(nameof(count), "The number of elements to dequeue cannot be negative.");
+
+        if (count > source.Count)
+            throw new ArgumentOutOfRangeException(nameof(count), "The number of elements to dequeue cannot be greater than the number of elements in the queue.");
+
+        if (!source.Any())
+            throw new InvalidOperationException("The queue is empty.");
 
         for (int i = 0; i < count; i++)
             yield return source.Dequeue();
