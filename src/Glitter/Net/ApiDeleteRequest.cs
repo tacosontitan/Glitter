1e4e288c508a56d216471cf6c16bf7e1bea33f23
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 21:21:25 -0600

    Renamed DeleteEndpoint to ApiDeleteRequest.

--- src/Glitter/Net/ApiDeleteRequest.cs
+++ src/Glitter/Net/ApiDeleteRequest.cs
@@ -1,22 +1,22 @@
 namespace Glitter.Net;
 
 /// <summary>
-/// Represents an <see cref="ApiEndpoint"/> utilizing <see cref="HttpMethod.Delete"/>.
+/// Represents an <see cref="ApiRequest"/> utilizing <see cref="HttpMethod.Delete"/>.
 /// </summary>
-public class DeleteEndpoint : ApiEndpoint
+public class ApiDeleteRequest : ApiRequest
 {
     /// <summary>
-    /// Creates a new <see cref="DeleteEndpoint"/> instance.
+    /// Creates a new <see cref="ApiDeleteRequest"/> instance.
     /// </summary>
     /// <param name="uri">A <see cref="string"/> representing the <see cref="System.Uri"/> of the endpoint.</param>
-    public DeleteEndpoint(string uriString) :
+    public ApiDeleteRequest(string uriString) :
         base(HttpMethod.Delete, uriString)
     { }
     /// <summary>
-    /// Creates a new <see cref="DeleteEndpoint"/> instance.
+    /// Creates a new <see cref="ApiDeleteRequest"/> instance.
     /// </summary>
     /// <param name="uri">The <see cref="System.Uri"/> of the endpoint.</param>
-    public DeleteEndpoint(Uri uri) :
+    public ApiDeleteRequest(Uri uri) :
         base(HttpMethod.Delete, uri)
     { }
 }
