4439011dde187fe1e95db26fe389d2b8959301c8
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 21:19:52 -0600

    Renamed PostEndpoint to ApiPostRequest.

--- src/Glitter/Net/ApiPostRequest.cs
+++ src/Glitter/Net/ApiPostRequest.cs
@@ -1,22 +1,22 @@
 namespace Glitter.Net;
 
 /// <summary>
-/// Represents an <see cref="ApiEndpoint"/> utilizing <see cref="HttpMethod.Post"/>.
+/// Represents an <see cref="ApiRequest"/> utilizing <see cref="HttpMethod.Post"/>.
 /// </summary>
-public class PostEndpoint : ApiEndpoint
+public class ApiPostRequest : ApiRequest
 {
     /// <summary>
-    /// Creates a new <see cref="PostEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPostRequest"/> instance.
     /// </summary>
     /// <param name="uri">A <see cref="string"/> representing the <see cref="System.Uri"/> of the endpoint.</param>
-    public PostEndpoint(string uriString) :
+    public ApiPostRequest(string uriString) :
         base(HttpMethod.Post, uriString)
     { }
     /// <summary>
-    /// Creates a new <see cref="PostEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPostRequest"/> instance.
     /// </summary>
     /// <param name="uri">The <see cref="System.Uri"/> of the endpoint.</param>
-    public PostEndpoint(Uri uri) :
+    public ApiPostRequest(Uri uri) :
         base(HttpMethod.Post, uri)
     { }
 }
