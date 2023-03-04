66b9deafad0bccecfee1dbcb383025b4cf33a589
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 21:22:09 -0600

    Renamed PatchEndpoint to ApiPatchRequest.

--- src/Glitter/Net/ApiPatchRequest.cs
+++ src/Glitter/Net/ApiPatchRequest.cs
@@ -1,22 +1,22 @@
 namespace Glitter.Net;
 
 /// <summary>
-/// Represents an <see cref="ApiEndpoint"/> utilizing <see cref="HttpMethod.Patch"/>.
+/// Represents an <see cref="ApiRequest"/> utilizing <see cref="HttpMethod.Patch"/>.
 /// </summary>
-public class PatchEndpoint : ApiEndpoint
+public class ApiPatchRequest : ApiRequest
 {
     /// <summary>
-    /// Creates a new <see cref="PatchEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPatchRequest"/> instance.
     /// </summary>
     /// <param name="uri">A <see cref="string"/> representing the <see cref="System.Uri"/> of the endpoint.</param>
-    public PatchEndpoint(string uriString) :
+    public ApiPatchRequest(string uriString) :
         base(new HttpMethod("PATCH"), uriString)
     { }
     /// <summary>
-    /// Creates a new <see cref="PatchEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPatchRequest"/> instance.
     /// </summary>
     /// <param name="uri">The <see cref="System.Uri"/> of the endpoint.</param>
-    public PatchEndpoint(Uri uri) :
+    public ApiPatchRequest(Uri uri) :
         base(new HttpMethod("PATCH"), uri)
     { }
 }
