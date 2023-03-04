88ed0953bc77dfe27153bf1feddeb5c5754b67f2
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 21:22:51 -0600

    Renamed PutEndpoint to ApiPutRequest.

--- src/Glitter/Net/ApiPutRequest.cs
+++ src/Glitter/Net/ApiPutRequest.cs
@@ -1,22 +1,22 @@
 namespace Glitter.Net;
 
 /// <summary>
-/// Represents an <see cref="ApiEndpoint"/> utilizing <see cref="HttpMethod.Put"/>.
+/// Represents an <see cref="ApiRequest"/> utilizing <see cref="HttpMethod.Put"/>.
 /// </summary>
-public class PutEndpoint : ApiEndpoint
+public class ApiPutRequest : ApiRequest
 {
     /// <summary>
-    /// Creates a new <see cref="PutEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPutRequest"/> instance.
     /// </summary>
     /// <param name="uri">A <see cref="string"/> representing the <see cref="System.Uri"/> of the endpoint.</param>
-    public PutEndpoint(string uriString) :
+    public ApiPutRequest(string uriString) :
         base(HttpMethod.Put, uriString)
     { }
     /// <summary>
-    /// Creates a new <see cref="PutEndpoint"/> instance.
+    /// Creates a new <see cref="ApiPutRequest"/> instance.
     /// </summary>
     /// <param name="uri">The <see cref="System.Uri"/> of the endpoint.</param>
-    public PutEndpoint(Uri uri) :
+    public ApiPutRequest(Uri uri) :
         base(HttpMethod.Put, uri)
     { }
 }
