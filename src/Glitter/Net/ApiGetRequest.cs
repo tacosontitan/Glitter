6d562badf2d7a495c2d87e0c94886a207ecf9061
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 21:20:26 -0600

    Renamed GetRequest to ApiGetRequest.

--- src/Glitter/Net/ApiGetRequest.cs
+++ src/Glitter/Net/ApiGetRequest.cs
@@ -3,20 +3,20 @@ namespace Glitter.Net;
 /// <summary>
 /// Represents an <see cref="ApiRequest"/> utilizing <see cref="HttpMethod.Get"/>.
 /// </summary>
-public class GetRequest : ApiRequest
+public class ApiGetRequest : ApiRequest
 {
     /// <summary>
-    /// Creates a new <see cref="GetRequest"/> instance.
+    /// Creates a new <see cref="ApiGetRequest"/> instance.
     /// </summary>
     /// <param name="uri">A <see cref="string"/> representing the <see cref="System.Uri"/> of the endpoint.</param>
-    public GetRequest(string uriString) :
+    public ApiGetRequest(string uriString) :
         base(HttpMethod.Get, uriString)
     { }
     /// <summary>
-    /// Creates a new <see cref="GetRequest"/> instance.
+    /// Creates a new <see cref="ApiGetRequest"/> instance.
     /// </summary>
     /// <param name="uri">The <see cref="System.Uri"/> of the endpoint.</param>
-    public GetRequest(Uri uri) :
+    public ApiGetRequest(Uri uri) :
         base(HttpMethod.Get, uri)
     { }
 }
