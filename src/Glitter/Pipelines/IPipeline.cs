c0b1da2326d976a70480f10e2fd6274ac9163ecb
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 22:13:48 -0600

    Added builder methods to IPipeline.

--- src/Glitter/Pipelines/IPipeline.cs
+++ src/Glitter/Pipelines/IPipeline.cs
@@ -11,4 +11,22 @@ public interface IPipeline<T>
     /// <param name="input">The input to the pipeline.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task Process(T input);
+    /// <summary>
+    /// Adds an action to the pipeline.
+    /// </summary>
+    /// <typeparam name="TAction">The type of the action to add.</typeparam>
+    /// <returns>The pipeline.</returns>
+    IPipeline<T> With<TAction>() where TAction : PipelineAction<T>;
+    /// <summary>
+    /// Adds a condition that must be met for the next action to be executed.
+    /// </summary>
+    /// <param name="condition">The condition to add.</param>
+    /// <returns>The pipeline.</returns>
+    IPipeline<T> When(Func<T, bool> condition);
+    /// <summary>
+    /// Provides an alternative action to execute if the previously specified condition is not met.
+    /// </summary>
+    /// <typeparam name="TAction">The type of the action to add.</typeparam>
+    /// <returns>The pipeline.</returns>
+    IPipeline<T> Else<TAction>() where TAction : PipelineAction<T>;
 }
