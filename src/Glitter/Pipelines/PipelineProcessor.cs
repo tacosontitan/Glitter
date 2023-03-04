7ae6b8663ac506b35eb890d2edf12e6d4ac988e4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-03 22:33:01 -0600

    Renamed PipelineAction to PipelineProcessor.

--- src/Glitter/Pipelines/PipelineProcessor.cs
+++ src/Glitter/Pipelines/PipelineProcessor.cs
@@ -1,15 +1,15 @@
 namespace Glitter.Pipelines;
 
 /// <summary>
-/// Represents a request handler that is executed as part of a pipeline.
+/// Represents a processor that is executed as part of a pipeline.
 /// </summary>
-public abstract class PipelineAction<T>
+public abstract class PipelineProcessor<T>
 {
     /// <summary>
-    /// Executes the pipeline action.
+    /// Executes the processor.
     /// </summary>
     /// <param name="input">The input to the pipeline.</param>
     /// <param name="next">The next action in the pipeline.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    public abstract Task Invoke(T input, PipelineAction<T> next);
+    public abstract Task Invoke(T input, PipelineProcessor<T> next, CancellationToken cancellationToken);
 }
