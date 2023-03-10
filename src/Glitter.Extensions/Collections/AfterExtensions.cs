83d6ff4e90e7aea3b33ef11f5ad15a5f8a04e90f
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-10 11:02:40 -0600

    Created direct equality overload for the After extension.

--- src/Glitter.Extensions/Collections/AfterExtensions.cs
+++ src/Glitter.Extensions/Collections/AfterExtensions.cs
@@ -32,4 +32,26 @@ public static class AfterExtensions
             .SkipWhile(element => !predicate(element))
             .Skip(1);
     }
+    /// <summary>
+    /// Returns a collection of elements from the specified collection that occur after the specified search value.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <param name="source">The collection to iterate over.</param>
+    /// <param name="searchValue">The value to search for.</param>
+    /// <returns>A collection of elements from the specified collection that occur after the specified search value.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="source"/> or <paramref name="searchValue"/> is <see langword="null"/>.
+    /// </exception>
+    public static IEnumerable<T> After<T>(this IEnumerable<T> source, T searchValue)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (searchValue is null)
+            throw new ArgumentNullException(nameof(searchValue));
+
+        return source
+            .SkipWhile(element => searchValue.Equals(element))
+            .Skip(1);
+    }
 }
