960525c628bccf2ac86e2db73c54db14cfdfb571
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-10 20:57:01 -0600

    Created ForEach extension that provides the previous and next element.

--- src/Glitter.Extensions/Collections/ForEachExtensions.cs
+++ src/Glitter.Extensions/Collections/ForEachExtensions.cs
@@ -28,4 +28,43 @@ public static class ForEachExtensions
         foreach (T? item in source)
             action(item);
     }
+    /// <summary>
+    /// Executes the specified action on each element of the collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <param name="source">The collection to iterate over.</param>
+    /// <param name="action">The action to execute on each element.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="source"/> or <paramref name="action"/> is <see langword="null"/>.
+    /// </exception>
+    /// <remarks>
+    /// The action is passed the current element, the previous element, and the next element.
+    /// </remarks>
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T?, T?, T?> action)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (action is null)
+            throw new ArgumentNullException(nameof(action));
+
+        T? previous = default;
+        T? current = default;
+        T? next = default;
+
+        using IEnumerator<T> enumerator = source.GetEnumerator();
+        if (enumerator.MoveNext())
+            current = enumerator.Current;
+
+        if (enumerator.MoveNext())
+            next = enumerator.Current;
+
+        while (current is not null)
+        {
+            action(previous, current, next);
+            previous = current;
+            current = next;
+            next = enumerator.MoveNext() ? enumerator.Current : default;
+        }
+    }
 }
