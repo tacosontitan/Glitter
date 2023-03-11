06976eaa049a7a62cefff9c2b9d757f852fe20d8
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-11 10:50:11 -0600

    Created test cases for ForEach extensions.

---
+++ test/Glitter.Extensions.Tests/Collections/ForEachExtensionsTests.cs
@@ -0,0 +1,46 @@
+namespace Glitter.Extensions.Tests.Collections;
+
+public class ForEachExtensionsTests
+{
+    [Fact]
+    public async Task SourceIsNull()
+    {
+        // Arrange
+        IEnumerable<int> source = null!;
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>(() => source!.ForEach(input => { }));
+        _ = Assert.Throws<ArgumentNullException>(() => source!.ForEach((previous, current, next) => { }));
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, CancellationToken.None, parallel: false)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, CancellationToken.None, parallel: true)).ConfigureAwait(false);
+    }
+    [Fact]
+    public async Task ActionIsNull()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>(() => source.ForEach((Action<int>)null!));
+        _ = Assert.Throws<ArgumentNullException>(() => source.ForEach((Action<int, int, int>)null!));
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, CancellationToken.None, parallel: false)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, CancellationToken.None, parallel: true)).ConfigureAwait(false);
+    }
+    [Fact]
+    public async Task ActionIsInvoked()
+    {
+        // Arrange
+        IEnumerable<int> source = new[] { 1, 2, 3 };
+        var expected = new List<int>();
+
+        // Act
+        source.ForEach(input => expected.Add(input));
+        source.ForEach((previous, current, next) => expected.Add(current));
+        await source.ForEach(input => expected.Add(input), CancellationToken.None, parallel: false).ConfigureAwait(false);
+        await source.ForEach(input => expected.Add(input), CancellationToken.None, parallel: true).ConfigureAwait(false);
+
+        // Assert
+        var expectedList = new List<int> { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+        Assert.Equal(expectedList, expected);
+    }
+}
