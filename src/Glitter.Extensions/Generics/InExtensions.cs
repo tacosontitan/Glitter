3e932b65c66bfd5d323f895e3de351cb4e899512
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-10 20:33:00 -0600

    Added overload for in extension to support custom comparisons.

--- src/Glitter.Extensions/Generics/InExtensions.cs
+++ src/Glitter.Extensions/Generics/InExtensions.cs
@@ -27,4 +27,27 @@ public static class InExtensions
         
         return values.Contains(value);
     }
+    /// <summary>
+    /// Determines whether the specified value is in a collection of specified values.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="value">The value to check.</param>
+    /// <param name="values">The values to check against.</param>
+    /// <returns><see langword="true"/> if the specified value is in the specified values; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="comparer"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// <paramref name="values"/> is <see langword="null"/> or empty.
+    /// </exception>
+    public static bool In<T>(this T value, IEqualityComparer<T> comparer, IEnumerable<T> values)
+    {
+        if (comparer is null)
+            throw new ArgumentNullException(nameof(comparer));
+
+        if (values?.Any() != true)
+            throw new ArgumentException("At least one value is required to evaluate the in clause.", nameof(values));
+        
+        return values.Contains(value, comparer);
+    }
 }
