cf48bbb3bcbb11b328aeab278c0b5225f162272c
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-13 22:39:39 -0500

    Added optional message for against null.

--- src/Glitter/Validation/AgainstNullExtensions.cs
+++ src/Glitter/Validation/AgainstNullExtensions.cs
@@ -10,14 +10,20 @@ public static class AgainstNullExtensions
     /// </summary>
     /// <typeparam name="T">The type of the value to guard.</typeparam>
     /// <param name="guardedValue">The value to guard.</param>
+    /// <param name="message">The message to use in the exception.</param>
     /// <returns>The value to guard.</returns>
     /// <exception cref="ArgumentNullException">
     /// <paramref name="guardedValue"/> is <see langword="null"/>.
     /// </exception>
-    public static GuardedValue<T> AgainstNull<T>(this GuardedValue<T> guardedValue)
+    public static GuardedValue<T> AgainstNull<T>(
+        this GuardedValue<T> guardedValue,
+        string? message = "The specified value cannot be null.")
     {
+        if (guardedValue is null)
+            throw new ArgumentNullException(nameof(guardedValue));
+
         if (guardedValue.Argument is null)
-            throw new ArgumentNullException(guardedValue.ArgumentName);
+            throw new ArgumentNullException(message, guardedValue.ArgumentName);
 
         return guardedValue;
     }
