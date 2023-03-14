d3a562df91c2dae4fb54d2115e8455a17b34ae5b
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-13 23:24:07 -0500

    Renamed Pipeline extension to CreatePipeline.

--- src/Glitter/Pipelines/CreatePipeline.cs
+++ src/Glitter/Pipelines/CreatePipeline.cs
@@ -3,13 +3,13 @@ namespace Glitter.Pipelines;
 /// <summary>
 /// Defines a method for creating a pipeline.
 /// </summary>
-public static class Pipeline
+public static class CreatePipeline
 {
     /// <summary>
     /// Creates a new pipeline for working with the specified type.
     /// </summary>
     /// <typeparam name="T">The type of the input for the pipeline.</typeparam>
     /// <param name="optimize">Indicates whether the pipeline should be optimized.</param>
-    public static IPipeline<T> CreateFor<T>(bool optimize = false) =>
+    public static IPipeline<T> For<T>(bool optimize = false) =>
         new Pipeline<T>(optimize);
 }
