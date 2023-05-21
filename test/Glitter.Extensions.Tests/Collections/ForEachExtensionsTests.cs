3161c065bcb0892adc7c32cd3460336b85084887
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-05-21 12:34:01 -0500

    Fixed method signatures in for each extensions tests.

--- test/Glitter.Extensions.Tests/Collections/ForEachExtensionsTests.cs
+++ test/Glitter.Extensions.Tests/Collections/ForEachExtensionsTests.cs
@@ -11,8 +11,8 @@ public class ForEachExtensionsTests
         // Assert
         _ = Assert.Throws<ArgumentNullException>(() => source!.ForEach(input => { }));
         _ = Assert.Throws<ArgumentNullException>(() => source!.ForEach((previous, current, next) => { }));
-        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, CancellationToken.None, parallel: false)).ConfigureAwait(false);
-        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, CancellationToken.None, parallel: true)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, parallel: false, CancellationToken.None)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source!.ForEach(input => { }, parallel: true, CancellationToken.None)).ConfigureAwait(false);
     }
 
     [Fact]
@@ -24,8 +24,8 @@ public class ForEachExtensionsTests
         // Assert
         _ = Assert.Throws<ArgumentNullException>(() => source.ForEach((Action<int>)null!));
         _ = Assert.Throws<ArgumentNullException>(() => source.ForEach((Action<int, int, int>)null!));
-        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, CancellationToken.None, parallel: false)).ConfigureAwait(false);
-        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, CancellationToken.None, parallel: true)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, parallel: false, CancellationToken.None)).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>(() => source.ForEach(action: null!, parallel: true, CancellationToken.None)).ConfigureAwait(false);
     }
     
     [Fact]
@@ -38,8 +38,8 @@ public class ForEachExtensionsTests
         // Act
         source.ForEach(expected.Add);
         source.ForEach((previous, current, next) => expected.Add(current));
-        await source.ForEach(expected.Add, CancellationToken.None, parallel: false).ConfigureAwait(false);
-        await source.ForEach(expected.Add, CancellationToken.None, parallel: true).ConfigureAwait(false);
+        await source.ForEach(expected.Add, parallel: false, CancellationToken.None).ConfigureAwait(false);
+        await source.ForEach(expected.Add, parallel: true, CancellationToken.None).ConfigureAwait(false);
 
         // Assert
         int testCount = 4;
