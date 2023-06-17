c924599a93e96595e0122ab874d22f762b128e78
tacosontitan <jkdavis1691@gmail.com>
2023-06-16 22:06:29 -0500

    Updated SqlRequest to implement ISqlRequest.

--- src/Glitter.Sql/Encapsulation/SqlRequest.cs
+++ src/Glitter.Sql/Encapsulation/SqlRequest.cs
@@ -19,8 +19,11 @@ namespace Glitter.Sql.Encapsulation;
 /// <summary>
 /// Defines a request for interacting with SQL.
 /// </summary>
-public class SqlRequest
+public class SqlRequest :
+    ISqlRequest
 {
+    private readonly List<SqlRequestParameter> _parameters;
+
     /// <summary>
     /// The command to execute.
     /// </summary>
@@ -29,32 +32,32 @@ public class SqlRequest
     /// <summary>
     /// The type of command to execute.
     /// </summary>
-    public CommandType CommandType { get; private set; }
+    public CommandType CommandType { get; set; }
     
     /// <summary>
     /// The parameters for the command.
     /// </summary>
-    public List<SqlRequestParameter> Parameters { get; private set; }
+    public IReadOnlyCollection<SqlRequestParameter> Parameters =>
+        _parameters.AsReadOnly();
+
+    /// <summary>
+    /// Creates a new <see cref="SqlRequest"/> instance.
+    /// </summary>
+    /// <param name="commandType">The type of command to execute.</param>
+    public SqlRequest(CommandType commandType)
+    {
+        CommandType = commandType;
+        _parameters = new List<SqlRequestParameter>();
+    }
     
     /// <summary>
     /// Creates a new <see cref="SqlRequest"/> instance.
     /// </summary>
     /// <param name="command">The command to execute.</param>
     /// <param name="commandType">The type of command to execute.</param>
-    public SqlRequest(string? command, CommandType commandType)
-    {
+    public SqlRequest(string? command, CommandType commandType) :
+        this(commandType) =>
         Command = command;
-        CommandType = commandType;
-        Parameters = new List<SqlRequestParameter>();
-    }
-    
-    /// <summary>
-    /// Creates a new <see cref="SqlRequest"/> instance.
-    /// </summary>
-    /// <param name="commandType">The command to execute.</param>
-    protected SqlRequest(CommandType commandType) :
-        this(null, commandType)
-    { }
     
     /// <summary>
     /// Adds a parameter to the request.
@@ -67,6 +70,9 @@ public class SqlRequest
     /// <param name="precision">The precision of the parameter.</param>
     /// <param name="scale">The scale of the parameter.</param>
     /// <typeparam name="T">The type of the parameter.</typeparam>
+    /// <returns>The <see cref="SqlRequest"/> instance.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="name"/> is null or whitespace.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when <paramref name="name"/> has already been specified.</exception>
     public virtual SqlRequest AddParameter<T>(
         string name,
         T? value,
@@ -76,16 +82,18 @@ public class SqlRequest
         byte? precision = null,
         byte? scale = null)
     {
-        Parameters.Add(new SqlRequestParameter(name, value, type, direction, size, precision, scale));
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException($"The name of the parameter cannot be null or whitespace.");
+
+        if (_parameters.Any(parameter => parameter.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
+            throw new InvalidOperationException($"The parameter `{name}` has already been specified.");
+            
+        _parameters.Add(new SqlRequestParameter(name, value, type, direction, size, precision, scale));
         return this;
     }
     
-    /// <summary>
-    /// Attempts to build the command for the request.
-    /// </summary>
-    /// <param name="command"></param>
-    /// <returns></returns>
-    public virtual bool TryBuildCommand(out string? command)
+    /// <inheritdoc/>
+    public virtual bool TryCompile(out string? command)
     {
         command = Command;
         return !string.IsNullOrWhiteSpace(command);
