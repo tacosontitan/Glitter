23ef209474b7372f8e95ae64804731c40062b4e6
tacosontitan <jkdavis1691@gmail.com>
2023-06-16 22:25:24 -0500

    Updated SqlFunction to leverage DefinedSqlRequest.

--- src/Glitter.Sql/Encapsulation/SqlFunction.cs
+++ src/Glitter.Sql/Encapsulation/SqlFunction.cs
@@ -19,68 +19,39 @@ namespace Glitter.Sql.Encapsulation;
 /// <summary>
 /// Represents a function within SQL.
 /// </summary>
-public class SqlFunction :
-    SqlRequest
+public abstract class SqlFunction :
+    DefinedSqlRequest
 {
-    private readonly string _schema;
-    private readonly string _functionName;
-    private readonly ICollection<string> _parameterNames;
-
     /// <summary>
     /// Creates a new <see cref="SqlFunction"/> instance.
     /// </summary>
     /// <param name="functionName">The name of the function being invoked.</param>
     /// <exception cref="ArgumentException"><paramref name="functionName"/> is null or whitespace.</exception>
     public SqlFunction(string functionName) :
-        base(CommandType.Text)
-    {
-        // Validate the function name.
-        if (string.IsNullOrWhiteSpace(functionName))
-            throw new ArgumentException("The function name cannot be null or whitespace.");
-
-        _schema = "dbo";
-        _functionName = functionName;
-        _parameterNames = new List<string>();
-    }
+        base(
+            schema: "dbo",
+            name: functionName,
+            commandType: CommandType.Text)
+    { }
 
     /// <summary>
     /// Creates a new <see cref="SqlFunction"/> instance.
     /// </summary>
     /// <param name="schema">The schema for the function.</param>
     /// <param name="functionName">The name of the function being invoked.</param>
-    /// <exception cref="ArgumentException"><paramref name="functionName"/> is null or whitespace.</exception>
+    /// <exception cref="ArgumentException"><paramref name="functionName"/> or <paramref name="functionName"/> is null or whitespace.</exception>
     public SqlFunction(string schema, string functionName) :
-        this(functionName) =>
-        _schema = schema;
+        base(
+            schema: schema,
+            name: functionName,
+            commandType: CommandType.Text)
+    { }
 
     /// <inheritdoc/>
-    /// <exception cref="ArgumentException"><paramref name="name"/> is <see langword="null"/>, whitespace, or already specified.</exception>
-    public override SqlRequest AddParameter<T>(
-        string name,
-        T? value,
-        DbType? type = null,
-        ParameterDirection? direction = null,
-        int? size = null,
-        byte? precision = null,
-        byte? scale = null) where T : default
+    public override bool TryCompile(out string? command)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException($"The name of the parameter cannot be null or whitespace.");
-
-        // If the parameter name is already listed then throw an exception.
-        if (_parameterNames.Contains(name))
-            throw new ArgumentException($"The parameter `{name}` has already been specified.");
-
-        _parameterNames.Add(name);
-        _ = base.AddParameter(name, value, type, direction, size, precision, scale);
-        return this;
-    }
-
-    /// <inheritdoc/>
-    public override bool TryBuildCommand(out string? command)
-    {
-        string parameters = string.Join(", ", _parameterNames.Select(x => $"@{x}"));
-        Command = $"SELECT * FROM [{_schema}].[{_functionName}] ({parameters})";
-        return base.TryBuildCommand(out command);
+        string parameters = string.Join(", ", Parameters.Select(x => $"@{x.Name}"));
+        Command = $"SELECT * FROM [{Schema}].[{Name}] ({parameters})";
+        return base.TryCompile(out command);
     }
 }
