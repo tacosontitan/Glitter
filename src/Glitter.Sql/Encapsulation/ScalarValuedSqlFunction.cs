bd110029877267aeb36f9360a6c81e518657faac
tacosontitan <jkdavis1691@gmail.com>
2023-06-16 22:25:57 -0500

    Created ScalarValuedSqlFunction.

---
+++ src/Glitter.Sql/Encapsulation/ScalarValuedSqlFunction.cs
@@ -0,0 +1,43 @@
+/*
+   Copyright 2023 tacosontitan and contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace Glitter.Sql.Encapsulation;
+
+/// <summary>
+/// Represents a scalar function within SQL.
+/// </summary>
+public abstract class ScalarValuedSqlFunction :
+    SqlFunction
+{
+    /// <summary>
+    /// Creates a new <see cref="ScalarValuedSqlFunction"/> instance.
+    /// </summary>
+    /// <param name="functionName">The name of the function being invoked.</param>
+    /// <exception cref="ArgumentException"><paramref name="functionName"/> is null or whitespace.</exception>
+    public ScalarValuedSqlFunction(string functionName) :
+        base(functionName)
+    { }
+
+    /// <summary>
+    /// Creates a new <see cref="ScalarValuedSqlFunction"/> instance.
+    /// </summary>
+    /// <param name="schema">The schema for the function.</param>
+    /// <param name="functionName">The name of the function being invoked.</param>
+    /// <exception cref="ArgumentException"><paramref name="functionName"/> or <paramref name="functionName"/> is null or whitespace.</exception>
+    public ScalarValuedSqlFunction(string schema, string functionName) :
+        base(schema, functionName)
+    { }
+}
