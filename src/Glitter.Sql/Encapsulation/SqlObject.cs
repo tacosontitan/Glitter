1b18d10f3da3b6d2db94889eba0a9d8e4b050dc0
tacosontitan <jkdavis1691@gmail.com>
2023-06-16 22:03:28 -0500

    Marked SqlObject as implementing ISqlObject.

--- src/Glitter.Sql/Encapsulation/SqlObject.cs
+++ src/Glitter.Sql/Encapsulation/SqlObject.cs
@@ -1,14 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+/*
+   Copyright 2023 tacosontitan and contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 namespace Glitter.Sql.Encapsulation;
 
 /// <summary>
 /// Represents an object within SQL.
 /// </summary>
-public abstract class SqlObject
+public abstract class SqlObject :
+    ISqlObject
 {
     /// <summary>
     /// Creates a new <see cref="SqlObject"/> instance.
