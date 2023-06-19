7dadaf70e9da0b0df26da8a79f06f3976aae38ee
tacosontitan <jkdavis1691@gmail.com>
2023-06-19 09:54:23 -0500

    Created interface for SqlTableAttribute.

---
+++ src/Glitter.Sql/Attributes/ISqlTableAttribute.cs
@@ -0,0 +1,39 @@
+/*
+   Copyright 2023 tacosontitan and contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// This interface is defining an attribute, so it should be named as such.
+// The reason for this convention is to improve proxy support for consumers who desire it.
+// This violates CA1711, but is something this repository stands firm on.
+// See: https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-type-members#nouns-and-noun-phrases
+#pragma warning disable CA1711
+
+namespace Glitter.Sql.Attributes;
+
+/// <summary>
+/// Defines a SQL table attribute.
+/// </summary>
+public interface ISqlTableAttribute
+{
+    /// <summary>
+    /// Gets or sets the name of the table.
+    /// </summary>
+    string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the schema of the table.
+    /// </summary>
+    string Schema { get; set; }
+}
