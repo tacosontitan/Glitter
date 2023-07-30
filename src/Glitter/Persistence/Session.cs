e06916540814025b6c3719cbacb50ea99fbcf7e7
tacosontitan <jkdavis1691@gmail.com>
2023-07-30 17:34:36 -0500

    Added session bag model.

---
+++ src/Glitter/Persistence/Session.cs
@@ -0,0 +1,102 @@
+﻿namespace Glitter.Persistence;
+
+/// <summary>
+/// Represents the current session.
+/// </summary>
+public static class Session
+{
+    private static SessionBag? _bag;
+    private static bool _initialized;
+    private static readonly object _bagLock = new();
+
+    /// <summary>
+    /// Initializes the session.
+    /// </summary>
+    public static void Initialize()
+    {
+        if (_initialized)
+            return;
+
+        _bag = new SessionBag();
+        _initialized = true;
+    }
+
+    /// <summary>
+    /// Initializes the session with the specified service provider.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider to use when attempting to resolve requests.</param>
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        if (_initialized)
+            return;
+
+        _bag = new SessionBag(serviceProvider);
+        _initialized = true;
+    }
+
+    /// <summary>
+    /// Gets the value associated with the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to get.</typeparam>
+    /// <returns>The value associated with the specified type.</returns>
+    public static T? Get<T>()
+    {
+        lock (_bagLock)
+        {
+            return _bag is null
+                ? throw new InvalidOperationException("The session has not been initialized.")
+                : _bag.Get<T>();
+        }
+    }
+
+    /// <summary>
+    /// Gets the value associated with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to get.</typeparam>
+    public static T? Get<T>(string key)
+    {
+        lock (_bagLock)
+        {
+            if (_bag is null)
+                throw new InvalidOperationException("The session has not been initialized.");
+
+            object? value = _bag[key];
+            return value is null
+                ? default
+                : (T)value;
+        }
+    }
+
+    /// <summary>
+    /// Sets the value associated with the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to set.</typeparam>
+    /// <param name="value">The value to set.</param>
+    public static void Set<T>(T value)
+    {
+        lock (_bagLock)
+        {
+            if (_bag is null)
+                throw new InvalidOperationException("The session has not been initialized.");
+
+            _bag.Set(value);
+        }
+    }
+
+    /// <summary>
+    /// Sets the value associated with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to set.</typeparam>
+    /// <param name="key">The key to associate with the value.</param>
+    /// <param name="value">The value to set.</param>
+    public static void Set<T>(string key, T value)
+    {
+        lock (_bagLock)
+        {
+            if (_bag is null)
+                throw new InvalidOperationException("The session has not been initialized.");
+
+            _bag[key] = value;
+        }
+    }
+}
---
+++ src/Glitter/Persistence/SessionBag.cs
@@ -0,0 +1,83 @@
+namespace Glitter.Persistence;
+
+/// <summary>
+/// Represents a bag of objects that are scoped to the current session.
+/// </summary>
+public class SessionBag
+{
+    private readonly IServiceProvider? _serviceProvider;
+    private readonly Dictionary<string, object?> _namedValues;
+    private readonly Dictionary<string, object?> _typedValues;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SessionBag"/> class.
+    /// </summary>
+    public SessionBag()
+    {
+        _namedValues = new Dictionary<string, object?>();
+        _typedValues = new Dictionary<string, object?>();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SessionBag"/> class.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider to use when attempting to resolve requests.</param>
+    public SessionBag(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _namedValues = new Dictionary<string, object?>();
+        _typedValues = new Dictionary<string, object?>();
+    }
+
+    /// <summary>
+    /// Gets the value associated with the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to get.</typeparam>
+    /// <returns>The value associated with the specified type.</returns>
+    public T? Get<T>()
+    {
+        if (_typedValues.TryGetValue(typeof(T).FullName!, out object? value))
+            return (T?)value;
+
+        if (_serviceProvider is not null)
+        {
+            object? service = _serviceProvider.GetService(typeof(T));
+            if (service != null)
+                return (T?)service;
+        }
+
+        return default;
+    }
+
+    /// <summary>
+    /// Sets the value associated with the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to set.</typeparam>
+    /// <param name="value">The value to set.</param>
+    public void Set<T>(T? value)
+    {
+        if (_typedValues.ContainsKey(typeof(T).FullName!))
+            _typedValues[typeof(T).FullName!] = value;
+        else
+            _typedValues.Add(typeof(T).FullName!, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to get or set.</param>
+    /// <returns>The value associated with the specified key.</returns>
+    public object? this[string key]
+    {
+        get => _namedValues.TryGetValue(key, out object? value)
+            ? value
+            : null;
+        set
+        {
+            if (_namedValues.ContainsKey(key))
+                _namedValues[key] = value;
+            else
+                _namedValues.Add(key, value);
+        }
+    }
+}
