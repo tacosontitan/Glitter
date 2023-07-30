338d24de77e46fc9f35b17567eeabbf6e434a74e
tacosontitan <jkdavis1691@gmail.com>
2023-07-30 18:59:19 -0500

    Removed generic validator.

--- src/Glitter/Validation/IValidator.cs
+++ src/Glitter/Validation/IValidator.cs
@@ -11,17 +11,3 @@ public interface IValidator
     /// <param name="value">The value to validate.</param>
     ValidationResult<object> Validate(object? value);
 }
-
-/// <summary>
-/// Defines methods for providing basic validation.
-/// </summary>
-/// <typeparam name="T">The type of the value to validate.</typeparam>
-public interface IValidator<T>
-    : IValidator
-{
-    /// <summary>
-    /// Validates the specified value.
-    /// </summary>
-    /// <param name="value">The value to validate.</param>
-    ValidationResult<T> Validate(T? value);
-}
