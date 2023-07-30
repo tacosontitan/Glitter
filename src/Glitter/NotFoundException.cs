111463218fad83c8fe2567e470fabb76a3d1bcde
tacosontitan <jkdavis1691@gmail.com>
2023-07-30 18:25:07 -0500

    Standardized not found exception.

--- src/Glitter/NotFoundException.cs
+++ src/Glitter/NotFoundException.cs
@@ -3,21 +3,26 @@ namespace Glitter;
 /// <summary>
 /// Represents an exception that is thrown when a resource is not found.
 /// </summary>
-public sealed class NotFoundException : Exception
+public sealed class NotFoundException
+    : Exception
 {
-    /// <summary>
-    /// Gets additional information about the exception.
-    /// </summary>
-    public string? AdditionalInformation { get; }
     /// <summary>
     /// Initializes a new instance of the <see cref="NotFoundException"/> class.
     /// </summary>
-    public NotFoundException() : this(null) { }
+    public NotFoundException()
+        : this(additionalInformation: null)
+    { }
+
     /// <summary>
     /// Initializes a new instance of the <see cref="NotFoundException"/> class.
     /// </summary>
     /// <param name="additionalInformation">Additional information about the exception.</param>
     public NotFoundException(string? additionalInformation) :
-        base("The requested resource was not found.") =>
+        base(message: "The requested resource was not found.") =>
         AdditionalInformation = additionalInformation;
+
+    /// <summary>
+    /// Gets additional information about the exception.
+    /// </summary>
+    public string? AdditionalInformation { get; }
 }
