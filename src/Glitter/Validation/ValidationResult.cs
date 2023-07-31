7f8e755dd917a3122b599807b9e2ef6e020b9e46
tacosontitan <jkdavis1691@gmail.com>
2023-07-30 19:22:41 -0500

    Created a simplified validation result model.

--- src/Glitter/Validation/ValidationResult.cs
+++ src/Glitter/Validation/ValidationResult.cs
@@ -3,24 +3,14 @@ namespace Glitter.Validation;
 /// <summary>
 /// Represents the result of a validation.
 /// </summary>
-/// <typeparam name="T">The type of the validated value.</typeparam>
-public class ValidationResult<T>
+public class ValidationResult
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="ValidationResult{T}"/> class.
+    /// Initializes a new instance of the <see cref="ValidationResult"/> class.
     /// </summary>
-    /// <param name="value">The validated value.</param>
     /// <param name="exceptions">The validation exceptions.</param>
-    public ValidationResult(T? value, IEnumerable<Exception>? exceptions)
-    {
-        Value = value;
+    public ValidationResult(IEnumerable<Exception>? exceptions) =>
         Exceptions = exceptions;
-    }
-
-    /// <summary>
-    /// Gets the validated value.
-    /// </summary>
-    public T? Value { get; }
 
     /// <summary>
     /// Gets the validation exceptions.
@@ -32,4 +22,26 @@ public class ValidationResult<T>
     /// </summary>
     public bool Successful =>
         Exceptions?.Any() != true;
+}
+
+/// <summary>
+/// Represents the result of a validation.
+/// </summary>
+/// <typeparam name="T">The type of the validated value.</typeparam>
+public class ValidationResult<T>
+    : ValidationResult
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationResult{T}"/> class.
+    /// </summary>
+    /// <param name="value">The validated value.</param>
+    /// <param name="exceptions">The validation exceptions.</param>
+    public ValidationResult(T? value, IEnumerable<Exception>? exceptions)
+        : base(exceptions) =>
+        Value = value;
+
+    /// <summary>
+    /// Gets the validated value.
+    /// </summary>
+    public T? Value { get; }
 }
