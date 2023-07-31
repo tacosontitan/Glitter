273e45171b6911a2559241594fa3672d99690d20
tacosontitan <jkdavis1691@gmail.com>
2023-07-30 19:30:26 -0500

    Updated target frameworks.

--- src/Glitter/Usings.cs
+++ src/Glitter/Usings.cs
@@ -1,2 +1,5 @@
-﻿global using System.Data;
-global using System.Diagnostics.CodeAnalysis;
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
