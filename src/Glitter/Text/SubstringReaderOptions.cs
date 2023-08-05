a82b7e4f1536f1b5da20184353ec6b1b758f5816
tacosontitan <jkdavis1691@gmail.com>
2023-08-04 19:46:08 -0500

    Created `SubstringReaderOptions`.

---
+++ src/Glitter/Text/SubstringReaderOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Glitter.Text;
+
+/// <summary>
+/// Represents options for a <see cref="SubstringReader"/>.
+/// </summary>
+public class SubstringReaderOptions
+{
+    /// <summary>
+    /// Creates new options for a <see cref="SubstringReader"/>.
+    /// </summary>
+    public SubstringReaderOptions()
+    {
+        FormatProvider = CultureInfo.InvariantCulture;
+        TrimOptions = null;
+    }
+
+    /// <summary>
+    /// Gets or sets the format provider for the <see cref="SubstringReader"/>.
+    /// </summary>
+    public IFormatProvider? FormatProvider { get; set; }
+
+    /// <summary>
+    /// Gets or sets the trimming options for the <see cref="SubstringReader"/>.
+    /// </summary>
+    public TrimOptions? TrimOptions { get; set; }
+}
