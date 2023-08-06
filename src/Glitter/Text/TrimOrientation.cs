6f7eb03f58b0bfb9624fe26fcfd5330d89aff344
tacosontitan <jkdavis1691@gmail.com>
2023-08-06 17:04:58 -0500

    Created `TrimOrientation`.

---
+++ src/Glitter/Text/TrimOrientation.cs
@@ -0,0 +1,22 @@
+﻿namespace Glitter.Text;
+
+/// <summary>
+/// Represents the orientation that should be used during trimming.
+/// </summary>
+public enum TrimOrientation
+{
+    /// <summary>
+    /// Indicates that trimming should be applied to both sides of the target.
+    /// </summary>
+    Full = 0,
+    
+    /// <summary>
+    /// Indicates that trimming should be applied to the left side of the target.
+    /// </summary>
+    Left = 1,
+    
+    /// <summary>
+    /// Indicates that trimming should be applied to the right side of the target.
+    /// </summary>
+    Right = 2
+}
