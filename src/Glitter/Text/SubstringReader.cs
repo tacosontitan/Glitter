b1921a3a0ad973a4c97ca09c8e0ac3e921b0b9b7
tacosontitan <jkdavis1691@gmail.com>
2023-08-06 16:24:24 -0500

    Added support for `SubstringReader.Peek`.

--- src/Glitter/Text/SubstringReader.cs
+++ src/Glitter/Text/SubstringReader.cs
@@ -1,5 +1,4 @@
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using Glitter.Serialization;
 
 namespace Glitter.Text;
 
@@ -40,11 +39,117 @@ public sealed class SubstringReader
     /// <summary>
     /// Gets the current index of the <see cref="SubstringReader"/>.
     /// </summary>
-    public int CurrentIndex => _currentIndex.Value;
+    public int CurrentPositions => _currentIndex.Value;
 
     /// <summary>
     /// Resets the current index to <c>0</c>.
     /// </summary>
     public void Reset() =>
         _currentIndex.Value = 0;
+
+    /// <summary>
+    /// Peeks forward to the end of the source string.
+    /// </summary>
+    /// <returns><see langword="null"/> if the current index beyond the end of the source string; otherwise, the substring from the current position of the reader to the end of the source string.</returns>
+    public string? Peek()
+    {
+        if (_currentIndex.Value > _source.Length)
+            return null;
+
+        return _source.Substring(_currentIndex.Value);
+    }
+
+    /// <summary>
+    /// Peeks forward to the end of the source string and attempts to convert the substring to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to convert the substring to.</typeparam>
+    /// <returns><see langword="default"/> for <typeparamref name="T"/> if the current index beyond the end of the source string; otherwise, the substring from the current position of the reader to the end of the source string.</returns>
+    /// <exception cref="FormatException">The resulting substring is not in the correct format for <typeparamref name="T"/>.</exception>
+    /// <exception cref="OverflowException">The resulting substring exceeds the range of <typeparamref name="T"/>.</exception>
+    public T? Peek<T>() where T : IConvertible
+    {
+        if (_currentIndex.Value > _source.Length)
+            return default;
+        
+        string substring = _source.Substring(_currentIndex.Value);
+        return (T)Convert.ChangeType(substring, typeof(T), _options.FormatProvider);
+    }
+    
+    /// <summary>
+    /// Peeks forward in the specified source string by the specified length and attempts to deserialize the substring to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to deserialize the substring to.</typeparam>
+    /// <param name="serializationProvider">The serialization provider to use for deserialization.</param>
+    /// <returns><see langword="default"/> for <typeparamref name="T"/> if the current index beyond the end of the source string; otherwise, the substring from the current position of the reader to the end of the source string.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="serializationProvider"/> is <see langword="null"/>.</exception>
+    public T? Peek<T>(ISerializationProvider serializationProvider)
+    {
+        if (serializationProvider is null)
+            throw new ArgumentNullException(nameof(serializationProvider));
+        
+        if (_currentIndex.Value > _source.Length)
+            return default;
+        
+        string substring = _source.Substring(_currentIndex.Value);
+        return serializationProvider.Deserialize<T>(substring, _options.FormatProvider);
+    }
+
+    /// <summary>
+    /// Peeks forward in the specified source string by the specified length.
+    /// </summary>
+    /// <param name="length">The length to peek forward.</param>
+    /// <returns>The substring from the current position of the reader to the end of the source string.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="length"/> is negative or exceeds the length of the source string when added to the current position of the reader.</exception>
+    public string? Peek(int length)
+    {
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "The length cannot be negative.");
+        
+        if (_currentIndex.Value + length > _source.Length)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "The length cannot exceed the length of the source string.");
+        
+        return _source.Substring(_currentIndex.Value, length);
+    }
+
+    /// <summary>
+    /// Peeks forward in the specified source string by the specified length and attempts to convert the substring to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to convert the substring to.</typeparam>
+    /// <param name="length">The length to peek forward.</param>
+    /// <returns>The substring from the current position of the reader to the end of the source string.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="length"/> is negative or exceeds the length of the source string when added to the current position of the reader.</exception>
+    /// <exception cref="FormatException">The resulting substring is not in the correct format for <typeparamref name="T"/>.</exception>
+    /// <exception cref="OverflowException">The resulting substring exceeds the range of <typeparamref name="T"/>.</exception>
+    public T? Peek<T>(int length) where T : IConvertible
+    {
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "The length cannot be negative.");
+        
+        if (_currentIndex.Value + length > _source.Length)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "The length cannot exceed the length of the source string.");
+        
+        string substring = _source.Substring(_currentIndex.Value, length);
+        return (T)Convert.ChangeType(substring, typeof(T), _options.FormatProvider);
+    }
+
+    /// <summary>
+    /// Peeks forward in the specified source string by the specified length and attempts to deserialize the substring to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to deserialize the substring to.</typeparam>
+    /// <param name="length">The length to peek forward.</param>
+    /// <param name="serializationProvider">The serialization provider to use for deserialization.</param>
+    public T? Peek<T>(int length, ISerializationProvider serializationProvider)
+    {
+        if (serializationProvider is null)
+            throw new ArgumentNullException(nameof(serializationProvider));
+        
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length), length, "The length cannot be negative.");
+
+        if (_currentIndex.Value + length > _source.Length)
+            return default;
+        
+        string substring = _source.Substring(_currentIndex.Value, length);
+        return serializationProvider.Deserialize<T>(substring, _options.FormatProvider);
+    }
 }
--- test/Glitter.Tests/Text/SubstringReaderTests.cs
+++ test/Glitter.Tests/Text/SubstringReaderTests.cs
@@ -9,12 +9,133 @@ public class SubstringReaderTests
     {
         // Arrange
         string? sample = null;
-        
+
         // Act
         void TestAction() =>
             _ = new SubstringReader(source: sample!);
 
         // Assert
         Assert.Throws<ArgumentException>(TestAction);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("\n")]
+    [InlineData("\r")]
+    public void Constructor_InvalidSource_ThrowsArgumentException(string? sample)
+    {
+        // Arrange & Act
+        void TestAction() =>
+            _ = new SubstringReader(source: sample!);
+
+        // Assert
+        Assert.Throws<ArgumentException>(TestAction);
+    }
+    
+    [Fact]
+    public void Peek_PriorToEndOfSource_ReturnsSubstring()
+    {
+        // Arrange
+        const string sample = "Hello, world!";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        string? result = reader.Peek();
+
+        // Assert
+        Assert.Equal(expected: sample, actual: result);
+    }
+    
+    [Fact]
+    public void Peek_NegativeLength_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        const string sample = "Hello, world!";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        void TestAction() =>
+            _ = reader.Peek(length: -1);
+
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(TestAction);
+    }
+    
+    [Fact]
+    public void Peek_ExceedsLengthOfSource_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        const string sample = "Hello, world!";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        void TestAction() =>
+            _ = reader.Peek(length: sample.Length + 1);
+
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(TestAction);
+    }
+    
+    [Fact]
+    public void Peek_ValidLength_ReturnsSubstring()
+    {
+        // Arrange
+        const int testLength = 5;
+        const string sample = "Hello, world!";
+        string expectedSubstring = sample.Substring(startIndex: 0, length: testLength);
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        string? result = reader.Peek(length: testLength);
+
+        // Assert
+        Assert.Equal(expected: expectedSubstring, actual: result);
+    }
+    
+    [Fact]
+    public void PeekAndConvert_TryConvertToInvalidType_ThrowsFormatException()
+    {
+        // Arrange
+        const string sample = "Hello, world!";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        void TestAction() =>
+            _ = reader.Peek<bool>();
+
+        // Assert
+        Assert.Throws<FormatException>(TestAction);
+    }
+    
+    [Fact]
+    public void PeekAndConvert_TryConvertToValidType_ReturnsConvertedValue()
+    {
+        // Arrange
+        const string sample = "123";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        int? result = reader.Peek<int>();
+
+        // Assert
+        Assert.Equal(expected: 123, actual: result);
+    }
+    
+    [Fact]
+    public void PeekAndConvert_SubstringExceedsRange_ThrowsOverflowException()
+    {
+        // Arrange
+        const string sample = "12345678";
+        var reader = new SubstringReader(source: sample);
+
+        // Act
+        void TestAction() =>
+            _ = reader.Peek<byte>();
+
+        // Assert
+        Assert.Throws<OverflowException>(TestAction);
     }
 }
