db16d1ab39329e1601889489750ebf487ef20bf9
tacosontitan <jkdavis1691@gmail.com>
2023-08-06 16:21:14 -0500

    Created `ISerializationProvider`.

---
+++ src/Glitter/Serialization/ISerializationProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Glitter.Serialization;
+
+/// <summary>
+/// Defines methods for serializing and deserializing objects.
+/// </summary>
+public interface ISerializationProvider
+{
+    /// <summary>
+    /// Serializes the specified value.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="formatProvider">An object providing culture-specific formatting information.</param>
+    /// <returns>The result of the serialization operation.</returns>
+    string Serialize<T>(T? value, IFormatProvider? formatProvider = null);
+    
+    /// <summary>
+    /// Deserializes the specified value.
+    /// </summary>
+    /// <param name="value">The value to deserialize.</param>
+    /// <param name="formatProvider">An object providing culture-specific formatting information.</param>
+    /// <returns>The result of the deserialization operation.</returns>
+    T? Deserialize<T>(string? value, IFormatProvider? formatProvider = null);
+}
