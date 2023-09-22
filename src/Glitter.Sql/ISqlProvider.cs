621eaf55c7362b571131b8e76fd9497ab37f29ac
tacosontitan <jkdavis1691@gmail.com>
2023-09-21 22:40:03 -0500

    Added explicit local methods to ISqlProvider.

--- src/Glitter.Sql/ISqlProvider.cs
+++ src/Glitter.Sql/ISqlProvider.cs
@@ -14,18 +14,50 @@
    limitations under the License.
 */
 
+using System.Threading;
 using Glitter.Sql.Encapsulation;
 
-namespace Glitter.Sql.Execution;
+namespace Glitter.Sql;
 
-public interface ISqlProvider<TRequest>
-    : IQueryHandler<TRequest>,
-      IScalarHandler<TRequest>,
-      INonQueryHandler<TRequest>
-    where TRequest : ISqlRequest
+/// <summary>
+/// Defines members for providing SQL services.
+/// </summary>
+public interface ISqlProvider
 {
     /// <summary>
     /// The <see cref="ISqlConnection"/> used by this service for interacting with SQL.
     /// </summary>
     ISqlConnection? ConnectionDefinition { get; set; }
+
+    /// <summary>
+    /// Executes a request and returns the results.
+    /// </summary>
+    /// <param name="request">The <see cref="ISqlRequest"/> to execute.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <typeparam name="T">The type of the value to return.</typeparam>
+    /// <returns>The results of the request.</returns>
+    Task<IEnumerable<T>> Execute<T>(
+        ISqlRequest request,
+        CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Executes a request and returns the number of rows affected.
+    /// </summary>
+    /// <param name="request">The <see cref="ISqlRequest"/> to execute.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <returns>The number of rows affected.</returns>
+    Task<int> ExecuteNonQuery(
+        ISqlRequest request,
+        CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Executes a request and returns the first column of the first row in the result set returned.
+    /// </summary>
+    /// <param name="request">The <see cref="ISqlRequest"/> to execute.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <typeparam name="T">The type of the value to return.</typeparam>
+    /// <returns>The first column of the first row in the result set.</returns>
+    Task<T> ExecuteScalar<T>(
+        ISqlRequest request,
+        CancellationToken cancellationToken = default);
 }
