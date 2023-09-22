b0f8f39a899fe2464574db89a538e2176a7ec64f
tacosontitan <jkdavis1691@gmail.com>
2023-09-22 16:43:29 -0500

    Added `System.Threading` to the global using directives.

--- src/Glitter.Sql/Usings.cs
+++ src/Glitter.Sql/Usings.cs
@@ -18,4 +18,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.Data;
 global using System.Linq;
+global using System.Threading;
 global using System.Threading.Tasks;
