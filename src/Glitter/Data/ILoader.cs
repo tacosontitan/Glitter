1f6c5020a384542a6dafa2801fb16021be8b1911
tacosontitan <jkdavis1691@gmail.com>
2023-10-04 12:51:58 -0500

    Created a generic loader interface.

---
+++ src/Glitter/Data/ILoader.cs
@@ -0,0 +1,31 @@
+/*
+   Copyright 2023 tacosontitan and contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace Glitter.Data;
+
+/// <summary>
+/// Represents a loader.
+/// </summary>
+/// <typeparam name="T">Specifies the type of the data to load.</typeparam>
+public interface ILoader<T>
+{
+    /// <summary>
+    /// Loads the data.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The loaded data.</returns>
+    Task<T> Load(CancellationToken cancellationToken = default);
+}
