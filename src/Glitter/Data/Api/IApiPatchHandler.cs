4cca258b64e68defc8a240b142415fd715a6278f
tacosontitan <jkdavis1691@gmail.com>
2023-10-12 22:19:12 -0500

    Created `IApiPatchHandler`.

---
+++ src/Glitter/Data/Api/IApiPatchHandler.cs
@@ -0,0 +1,19 @@
+namespace Glitter.Data.Api;
+
+/// <summary>
+/// Represents a handler for a PATCH request.
+/// </summary>
+/// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+public interface IApiPatchHandler<in TRequest>
+    : IDataRequestHandler<IApiProvider, TRequest>
+    where TRequest : IRequest;
+    
+/// <summary>
+/// Represents a handler for a PATCH request.
+/// </summary>
+/// <typeparam name="TProvider">Specifies the type of the provider.</typeparam>
+/// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+public interface IApiPatchHandler<in TProvider, in TRequest>
+    : IDataRequestHandler<TProvider, TRequest>
+    where TProvider : IApiProvider
+    where TRequest : IRequest;
