b60dd6166b24dfa5221d89d22752e6d9821efb3f
tacosontitan <jkdavis1691@gmail.com>
2023-10-12 20:35:20 -0500

    Removed redundant assignment of `false` to `ValidationOptions.TreatWarningsAsFailures`.

--- src/Glitter/Validation/ValidationOptions.cs
+++ src/Glitter/Validation/ValidationOptions.cs
@@ -23,5 +23,5 @@ public class ValidationOptions
     : IValidationOptions
 {
     /// <inheritdoc />
-    public bool TreatWarningsAsFailures { get; set; } = false;
+    public bool TreatWarningsAsFailures { get; set; }
 }
