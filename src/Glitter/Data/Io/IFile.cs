af9dda016d83393c397e2ac041877914df91a3f4
tacosontitan <jkdavis1691@gmail.com>
2023-10-12 20:37:41 -0500

    Created `IFile`.

---
+++ src/Glitter/Data/Io/IFile.cs
@@ -0,0 +1,22 @@
+﻿namespace Glitter.Data.Io;
+
+/// <summary>
+/// Represents a file.
+/// </summary>
+public interface IFile
+{
+    /// <summary>
+    /// Gets or sets the name of the file.
+    /// </summary>
+    string Name { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the extension of the file.
+    /// </summary>
+    string Extension { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the content of the file.
+    /// </summary>
+    IEnumerable<byte> Content { get; set; }
+}
