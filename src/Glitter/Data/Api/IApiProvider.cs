48dc576fd400887cbed9b2e92b2636860313782a
tacosontitan <jkdavis1691@gmail.com>
2023-10-12 22:14:22 -0500

    Updated signature for `IApiProvider`.

--- src/Glitter/Data/Api/IApiProvider.cs
+++ src/Glitter/Data/Api/IApiProvider.cs
@@ -1,7 +1,93 @@
 namespace Glitter.Data.Api;
 
+/// <summary>
+/// Represents a provider for an API.
+/// </summary>
 public interface IApiProvider
     : IDataProvider
 {
+    /// <summary>
+    /// Gets the specified resource.
+    /// </summary>
+    /// <typeparam name="TResponse">Specifies the type of the response.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>The response from the API.</returns>
+    Task<TResponse> Get<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
     
+    /// <summary>
+    /// Posts the specified resource.
+    /// </summary>
+    /// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Post<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest;
+    
+    /// <summary>
+    /// Posts the specified resource.
+    /// </summary>
+    /// <typeparam name="TResponse">Specifies the type of the response.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>The response from the API.</returns>
+    Task<TResponse> Post<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Patches the specified resource.
+    /// </summary>
+    /// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Patch<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest;
+    
+    /// <summary>
+    /// Patches the specified resource.
+    /// </summary>
+    /// <typeparam name="TResponse">Specifies the type of the response.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>The response from the API.</returns>
+    Task<TResponse> Patch<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Updates or creates the specified resource.
+    /// </summary>
+    /// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Put<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest;
+    
+    /// <summary>
+    /// Updates or creates the specified resource.
+    /// </summary>
+    /// <typeparam name="TResponse">Specifies the type of the response.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>The response from the API.</returns>
+    Task<TResponse> Put<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Deletes the specified resource.
+    /// </summary>
+    /// <typeparam name="TRequest">Specifies the type of the request.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Delete<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest;
+    
+    /// <summary>
+    /// Deletes the specified resource.
+    /// </summary>
+    /// <typeparam name="TResponse">Specifies the type of the response.</typeparam>
+    /// <param name="request">The request to send to the API.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
+    /// <returns>The response from the API.</returns>
+    Task<TResponse> Delete<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
 }
